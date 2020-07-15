dade6b8fb655ac8b86162c523b6eeb2910801d30
treCoops <trevogayan1995@gmail.com>
2020-07-15 19:08:01 +0530

    Initial Commit

---
+++ iOS Assignment 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Assignment 1
+//
+//  Created by treCoops on 7/15/20.
+//  Copyright © 2020 treCoops. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS Assignment 1Tests/iOS_Assignment_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_Assignment_1Tests.swift
+//  iOS Assignment 1Tests
+//
+//  Created by treCoops on 7/15/20.
+//  Copyright © 2020 treCoops. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Assignment_1
+
+class iOS_Assignment_1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
